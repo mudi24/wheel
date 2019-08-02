b07b191ed68fdadd6681be99ea75a9ce62d17119
yaokai <yaokai729@outlook.com>
2019-08-02 22:57:50 +0800

    下安装parcel，把button抽离为单独的组件，以便复用

---
+++ src/button.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="g-button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang='scss' scoped>
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover {
+    border-color: var(--button-active-bg);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+}
+</style>
